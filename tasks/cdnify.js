865119e510f6da208d007eeb532a20fe9c66fd66
mhaligowski <mhaligowski@gmail.com>
2014-02-25 22:38:13 +0100

    Write the contents of the task

--- tasks/cdnify.js
+++ tasks/cdnify.js
@@ -7,15 +7,32 @@
  */
 
 'use strict';
+var fs = require('fs');
+var path = require('path');
+
+var googlecdn = require('google-cdn');
 
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('cdnify', 'Grunt task for substituting JS libraries with CDN versions', function() {
+    var done = this.async();
+
+    var bowerPath = path.resolve(this.data.bower);
+    var bowerConfig = grunt.file.readJSON(bowerPath);
+
     var options = this.options({
+        cdn: require('google-cdn-data'),
+        components_path: 'bower_components'
     });
 
-    
-
+    this.data.html.forEach(function(filename) {
+        var markup = grunt.file.read(filename);
+        googlecdn(markup, bowerConfig, options, function(err, result) {
+            if (err) { throw err; }
+                
+            grunt.file.write(filename, result);
+            done();
+        });
+    });   
   });
-
 };
